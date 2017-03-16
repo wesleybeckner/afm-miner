c2af86687680e48c6889bd89a58ba56986eea9c0
Jessica Kong <kongjy@uw.edu>
2017-03-15 21:07:39 -0700

    unit test for alignimages.py jk

---
+++ afmMiner/test_alignimages.py
@@ -0,0 +1,21 @@
+mport numpy as np 
+from alignimages import alignImage
+   
+#define path of data files 
+MAPI01HeightPath='../afmMiner/data/MAPI_01.txt'
+MAPI00HeightPath='../afmMiner/data/MAPI_00.txt'
+#load in txt files corresponding to height
+MAPI01Height=np.loadtxt(MAPI01HeightPath, delimiter="\t", dtype='float')
+MAPI00Height=np.loadtxt(MAPI00HeightPath, delimiter="\t", dtype='float')
+
+
+def test_alignimage():
+
+    #define inputs
+    static=MAPI00Height
+    moving=MAPI01Height
+    level_iters = [200, 100, 50, 25]
+
+    assert static.shape[0]==moving.shape[0] and static.shape[1]==moving.shape[1]
+    return 'Static and Moving have the same number of rows and columns!'
+
